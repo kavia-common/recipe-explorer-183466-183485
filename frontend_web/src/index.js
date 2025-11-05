b2356c1e7414dc10f698bdaf9aca0ad0bd55dea7
Kavia AI <production@kavia.ai>
2025-11-05 13:05:31 +0000

    Checkpoint 5 - cg1333d82c

--- frontend_web/src/index.js
+++ frontend_web/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './styles/signin-isolation.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
